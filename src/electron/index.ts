ec1bbc54c859be687a7cf699e24616999c504528
rixcian <rostislav.kremecek@protonmail.com>
2024-10-07 14:12:08 +0200

    wip: debug mode

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -18,23 +18,54 @@ const config = new ElectronStore();
 let started = false;
 
 // To be called from the main process
-function setup(webContents: WebContents): void {
+function setup(webContents: WebContents, debug: boolean = false, debugLog: (message?: any, ...optionalParams: any[]) => void = console.log): void {
   // Will be called by the renderer process
   ipcMain.on(START_NOTIFICATION_SERVICE, async (_, firebaseCredentials: FirebaseCredentials) => {
+    if (debug) debugLog('[electron/index.ts] [info] Starting notification service');
+    
     // Retrieve saved credentials
     let credentials: any = config.get('credentials');
+
+    // Log notification token if debug is true
+    if (debug) {
+      if (credentials && credentials.fcm && credentials.fcm.token) {
+        debugLog(`[electron/index.ts] [info] Notification token: ${credentials.fcm.token}`);
+      } else {
+        debugLog('[electron/index.ts] [info] No notification token found');
+      }
+    }
+    
     // Retrieve saved senderId
     const savedFirebaseCredentials: FirebaseCredentials | undefined = config.get('firebaseCredentials') as
       | FirebaseCredentials
       | undefined;
+    
+    // Log saved Firebase credentials if debug is true
+    if (debug) {
+      if (savedFirebaseCredentials) {
+        debugLog(`[electron/index.ts] Saved Firebase Credentials (from electron-config): ${savedFirebaseCredentials}`);
+      } else {
+        debugLog('[electron/index.ts] No saved Firebase credentials found');
+      }
+      if (credentials) {
+        debugLog(`[electron/index.ts] Starting notification service; Credentials (from electron-config): ${credentials}`);
+      }
+    }
+    
     if (started) {
+      if (debug) debugLog('[electron/index.ts] Notification service already started');
       webContents.send(NOTIFICATION_SERVICE_STARTED, (credentials?.fcm || {}).token);
       return;
     }
+
     started = true;
+    
     try {
       // Retrieve saved persistentIds : avoid receiving all already received notifications on start
       const persistentIds: string[] = (config.get('persistentIds') as string[]) || [];
+
+      if (debug) debugLog(`[electron/index.ts] Persistent ids: ${persistentIds}`);
+
       // Register if no credentials or if senderId has changed
       if (
         !credentials ||
@@ -44,7 +75,12 @@ function setup(webContents: WebContents): void {
         savedFirebaseCredentials.projectId !== firebaseCredentials.projectId ||
         savedFirebaseCredentials.vapidKey !== firebaseCredentials.vapidKey
       ) {
+        if (debug) debugLog('[electron/index.ts] No cached credentials found! Registering with new firebase credentials');
+
         credentials = await register(firebaseCredentials);
+
+        if (debug) debugLog(`[electron/index.ts] register() func finished; Credentials: ${credentials}`);
+        
         // Save credentials for later use
         config.set('credentials', credentials);
         // Save senderId
